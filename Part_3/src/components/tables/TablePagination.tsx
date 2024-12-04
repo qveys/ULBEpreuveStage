30ae3071f440a55e4fbf15ffce68e3afa24b1e02
qveys <quentinveys@gmail.com>
2024-12-04 18:27:29 +0100

    P3 | TablePagination
    (cherry picked from commit ef7917d06173554c693df52b5d79875b77004cbd)

---
+++ Part_3/src/components/tables/TablePagination.tsx
@@ -0,0 +1,93 @@
+import {ChevronLeftIcon, ChevronRightIcon} from "lucide-react"
+import {Table} from "@tanstack/react-table"
+
+import {Button} from "@/components/ui/Button"
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/Select"
+
+interface TablePaginationProps<TData> {
+  table: Table<TData>
+}
+/**
+ * Component Properties for TablePagination
+ * @template TData - The type of data the table is handling.
+ * @property {Table<TData>} table - The table instance provided by react-table,
+ *   which is used for handling pagination logic and state.
+ */
+export function TablePagination<TData>({
+  table,
+}: TablePaginationProps<TData>) {
+  return (
+    <div className="flex items-center justify-between px-2 w-full">
+      <div className="flex-1 flex items-center justify-between">
+        <div className="flex items-center space-x-2">
+          <p className="text-sm font-medium">Rows per page</p>
+          <Select
+            value={`${table.getState().pagination.pageSize}`}
+            onValueChange={(value) => {
+              table.setPageSize(Number(value));
+            }}
+          >
+            <SelectTrigger className="h-8 w-[70px]">
+              <SelectValue placeholder={table.getState().pagination.pageSize} />
+            </SelectTrigger>
+            <SelectContent side="top">
+              {[10, 20, 30].map((pageSize) => (
+                <SelectItem key={pageSize} value={`${pageSize}`}>
+                  {pageSize}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+
+        <div className="text-sm font-medium">
+          Page {table.getState().pagination.pageIndex + 1} of{" "}
+          {table.getPageCount()}
+        </div>
+
+        <div className="flex items-center space-x-2">
+          {/*
+          <Button
+            variant="outline"
+            className="h-8 w-8 p-0"
+            onClick={() => table.setPageIndex(0)}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <span className="sr-only">Go to first page</span>
+            <ChevronsLeftIcon className="h-4 w-4" />
+          </Button>
+          */}
+          <Button
+            variant="outline"
+            className="h-8 w-8 p-0"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <span className="sr-only">Go to previous page</span>
+            <ChevronLeftIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="outline"
+            className="h-8 w-8 p-0"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            <span className="sr-only">Go to next page</span>
+            <ChevronRightIcon className="h-4 w-4" />
+          </Button>
+          {/*
+          <Button
+            variant="outline"
+            className="h-8 w-8 p-0"
+            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+            disabled={!table.getCanNextPage()}
+          >
+            <span className="sr-only">Go to last page</span>
+            <ChevronsRightIcon className="h-4 w-4" />
+          </Button>
+          */}
+        </div>
+      </div>
+    </div>
+  );
+}
