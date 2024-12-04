9523e604a5f27191b6450fa197deb54bb2d6f05e
qveys <quentinveys@gmail.com>
2024-12-04 13:10:41 +0100

    P3 | Constante Menu

---
+++ Part_3/src/constants/menu.ts
@@ -0,0 +1,14 @@
+import {NavigationItem} from "@/interfaces/NavigationItem";
+
+/**
+ * An array of navigation items for the menu.
+ */
+export const MENU : NavigationItem[] = [
+      { title: 'Statistiques', href: '/stats' },
+      { title: 'Classement', href: '/classement' },
+      { title: 'Hopitaux', href: '/hopitaux' },
+      { title: 'Services', href: '/services' },
+      { title: 'Places', href: '/places' },
+      { title: 'Préférences', href: '/preferences' },
+      { title: 'Résultats', href: '/resultats' },
+  ];
