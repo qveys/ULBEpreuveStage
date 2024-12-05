5d9d2f99cb93daeb9d8a5f13132cebf61e9f9f7a
qveys <quentinveys@gmail.com>
2024-12-05 16:17:11 +0100

    P2 | Stage
    (cherry picked from commit 344581643b99092cde1b5a3c8d4a534e363eb3bc)

---
+++ Part_2/6_Implémentation/interfaces/APP/stage.interface.ts
@@ -0,0 +1,9 @@
+/**
+ * Définition des types de base
+ */
+interface Stage {
+    hopital: number;
+    service: number;
+    capacite: number;
+    assignations: string[];  // Liste des matricules assignés
+}
